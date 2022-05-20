c65c09a515b49537b4a8062e2abc9e95e6f34493
AlexandraElena07 <alexandraelenasirbulescu@gmail.com>
2022-05-20 20:46:06 +0300

    FIS-32 merge

--- src/main/java/com/example/projectfis/HelloApplication.java
+++ src/main/java/com/example/projectfis/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.projectfis;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login1.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene((Parent)fxmlLoader.load());
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.setTitle("Autentificare");
